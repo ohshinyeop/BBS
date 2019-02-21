8811202167540f5dc4a6bc231e48cc824fd9433b
ohshinyeop <46528849+ohshinyeop@users.noreply.github.com>
2019-02-21 22:37:04 +0900

    BBS - join,join action
    connect- join.jsp & joinaction.jsp 
    add method join at userDAO

--- BBS/src/user/UserDAO.java
+++ BBS/src/user/UserDAO.java
@@ -38,17 +38,18 @@ public class UserDAO {
 			System.out.println("rs : "+ rs);
 			//////
 			
-			if(rs.next()) {
+			if(rs.next()) {   //아이디가 있다
 				
 				System.out.println(rs.getString(1));
 				System.out.println(userPassword);
 				
 				if(rs.getString(1).equals(userPassword)) {
-					return 1;
+					return 1; //비밀번호 일치
 				}
 				else {
 					System.out.println("비밀번호 불일치");
 					return 0; //비밀번호 불일치
+					
 				}
 			}
 			return -1; //아이디가 없음
@@ -56,5 +57,20 @@ public class UserDAO {
 			e.printStackTrace();
 		}
 		return -2;  //데이터베이스 오류
+	}
+	public int join(User user) {
+		String SQL = "INSERT INTO USER VALUES (?,?,?,?,?)";
+		try {
+			pstmt = conn.prepareStatement(SQL);
+			pstmt.setString(1,user.getUserID());
+			pstmt.setString(2,user.getUserPassword());
+			pstmt.setString(3,user.getUserName());
+			pstmt.setString(4,user.getUserGender());
+			pstmt.setString(5,user.getUserEmail());
+			return pstmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return -1;
 	}
 }
